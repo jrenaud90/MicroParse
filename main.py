8ed2be23c3f70a49037622a8c9e6dc236c3d0256
Joe Renaud <jrenaud@masonlive.gmu.edu>
2014-08-11 00:44:38 -0400

    0.4.8n

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ Created on Mon Jul 28 21:08:13 2014
 
 from HTMLParser import HTMLParser
 import os,urllib2,datetime
-vers = '0.4.5n'
-def LogMaker(CurrentPath):
-    logpath = CurrentPath + 'Output.log'
-    log = open(logpath,'w')
-    #
-    return log
+vers = '0.4.8n'
+####Main Files####
+#==============================================================================
+# Run MainGrab() if you want to run the parser from scratch, downloading new 
+# files.
+#
+# Run OnlyParse if you do not want to download files. However, follow these instructions:
+#  - First make a folder with the data you want to parse saved as 'htmlparse.tmp'
+#  - Record the file path to the main folder holing the htmlparse.tmp, this is 
+#  - where all data will be saved.
+#  - Run:: 'log = logMaker(path)' where path is the aforementioned path
+#  - Now run:: 'OnlyParse(path,log)' where path is as mentioned, and log is the 
+#  - variable holding the logMaker object.
+#==============================================================================
 def MainGrab():
     FolderPath = 'C:/MicroLensParse/'
     if not os.path.exists(FolderPath):
@@ -37,9 +45,10 @@ def MainGrab():
 def OnlyParse(Path,log):
     daysOut = 30 #Days out from today to search
     precision = 60 #minutes, I would keep this between 30 - 240
-    ObservLong = 38.8308 #degrees North
-    ObservLat = 77.3075 #degrees West
+    ObservLong = -77.305325 #degrees East
+    ObservLat = 38.828176 #degrees North
     MinMag = 20 #Minimum Magnitude to detect
+    minALT = 15 #Degrees above the horizon
     compactPrintout = 'y'
     log.write('Previous Log file found\n')
     log.write('OnlyParse function is now running...\n')
@@ -60,11 +69,17 @@ def OnlyParse(Path,log):
     log.write('Running through events and times')
     for event in events:
         for time in times:
-            if event.parse(ObservLong,ObservLat,time,MinMag):
+            if event.parse(ObservLong,ObservLat,time,MinMag,minALT):
                 event.print2CSV(compactPrintout,Path,time)
     log.write('Parser Finished')
-        
     
+#######Back Ground Files#######
+    
+def LogMaker(CurrentPath):
+    logpath = CurrentPath + 'Output.log'
+    log = open(logpath,'w')
+    #
+    return log    
 def inputintoobject(data,log):
     log.write('CSVfile made\n')
     lines=data.readlines()
@@ -129,34 +144,6 @@ def ParseDataFile(htmldata,datastore,log,Path):
             newitem = 0;       
     log.write('--- '+ str(newevents)+ ' New Event(s) Found!\n')
     log.write('--- ' + str(actives) + ' Active Event(s) Found!\n')
-def CurrentUTC():
-    #Needs imported datetime
-    x = datetime.datetime.utcnow()
-    return x
-def UTC2str(UTC):
-    y=str(UTC.year)
-    if UTC.month <10:
-        m = '0' + str(UTC.month)
-    else:
-        m = str(UTC.month)
-    if UTC.day <10:
-        d = '0' + str(UTC.day)
-    else: 
-        d = str(UTC.day)
-    if UTC.hour <10:
-        hr = '0' + str(UTC.hour)
-    else:
-        hr = str(UTC.hour)
-    if UTC.minute <10:
-        mn = '0' + str(UTC.minute)
-    else:
-        mn = str(UTC.minute)
-    if UTC.second <10:
-        sc = '0' + str(UTC.second)
-    else:
-        sc = str(UTC.second)
-    Full = m+d+y+'-'+hr+mn+sc
-    return Full
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         if len(data) > 1:
@@ -188,8 +175,87 @@ def DownloadHTMLtext(html,log,CurrentPath):
             pass
         log.write('HTML data passed into super-string\n')
         return kstr
+def CurrentUTC():
+    #Needs imported datetime
+    x = datetime.datetime.utcnow()
+    return x
+def UTC2str(UTC):
+    y=str(UTC.year)
+    if UTC.month <10:
+        m = '0' + str(UTC.month)
+    else:
+        m = str(UTC.month)
+    if UTC.day <10:
+        d = '0' + str(UTC.day)
+    else: 
+        d = str(UTC.day)
+    if UTC.hour <10:
+        hr = '0' + str(UTC.hour)
+    else:
+        hr = str(UTC.hour)
+    if UTC.minute <10:
+        mn = '0' + str(UTC.minute)
+    else:
+        mn = str(UTC.minute)
+    if UTC.second <10:
+        sc = '0' + str(UTC.second)
+    else:
+        sc = str(UTC.second)
+    Full = m+d+y+'-'+hr+mn+sc
+    return Full
+def dateFinder(NowQ,days,precision):
+    #days - out from now
+    #precision in minutes 
+    starthourUTC = '00:00'
+    endhourUTC = '09:00'
+    daysinmonths = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+    #Note, code does not account for leap years at this stage.
+    if NowQ == 'y':
+        now = datetime.datetime.utcnow()
+        syear = now.year
+        smonth = now.month
+        sday = now.day
+        if now.hour >= 0 and now.hour <= 4:
+            if sday == 1:
+                if smonth == 1:
+                    syear = syear -1
+                    smonth = 12
+                else:
+                    smonth = smonth - 1
+                sday = daysinmonths[smonth]
+            else:
+                sday = sday - 1
+        shour = int(starthourUTC[0:2])
+        smin = int(starthourUTC[3:len(starthourUTC)])
+        ehour = int(endhourUTC[0:2])
+        emin = int(endhourUTC[3:len(endhourUTC)])
+        thour = shour
+        tmin = smin
+        tday = sday
+        tmonth = smonth
+        tyear = syear
+        UTCtimes = []
+        for i in range(1,days):
+            thour = shour
+            tmin = smin
+            if tday > daysinmonths[tmonth]:
+                tday = tday - daysinmonths[tmonth]
+                tmonth = tmonth + 1
+                if tmonth > 12:
+                    tmonth = 1
+                    tyear = tyear + 1
+            while thour < ehour or tmin < emin:
+                UTCtimes.append(datetime.datetime(tyear,tmonth,tday,thour,tmin))
+                tmin = tmin+precision
+                while tmin > 59:
+                    thour = thour + 1
+                    tmin = tmin - 60
+            tday = tday + 1
+    else:
+        print 'Sorry, this feature is not yet implemented.'
+    return UTCtimes
 class microevent:
-	version = '0.2.5n'
+	version = '0.2.7n'
 	versionDate = '8-10-2014'
 	def __init__(self,a,u,f,s,r,d,tmj,tmu,umn,tu,am,dma,fbl,ibl,io):
 		self.active = a
@@ -242,9 +308,11 @@ class microevent:
 			self.u_min = value
 		else:
 			print 'Invalid string'
-	def parseVisibility(self,lonng,lat,UTC):
-          RA = DMStoDeg(self.ra,'n')
-          DEC = DMStoDeg(self.dec,'n')
+	def parseVisibility(self,lonng,lat,UTC,minALT):
+          RA = RAtoHours_Deg(self.ra,'y')
+          DEC = DECtoDeg(self.dec,'n')
+          time = datetime2String_Num(UTC,'hx')
+          LST = LST_finder(daysFromJ2000(UTC),time,lonng):
 		#WORK NEEDS DONE
 		#source? http://www.stargazing.net/kepler/altaz.html
 		#%if condition:Visibility = 1
@@ -259,14 +327,14 @@ class microevent:
 		else:
 			MinMagTest = False
 		return MinMagTest
-	def parse(self,lonng,lat,utc,minmag):
-		if (minmag == 'NA' and self.parseVisibility(lonng,lat,utc)):
-              parsepass = True
-		elif (self.parseMinimumMag(minmag) and self.parseVisibility(lonng,lat,utc)):
-              parsepass = True
-          else:
-              parsepass = False
-		return parsepass
+	def parse(self,lonng,lat,utc,minmag,minALT):
+         if (minmag == 'NA' and self.parseVisibility(lonng,lat,utc,minALT)):
+             parsepass = True
+         elif (self.parseMinimumMag(minmag) and self.parseVisibility(lonng,lat,utc)):
+             parsepass = True
+         else:
+             parsepass = False
+         return parsepass
 	def print2CSV(self,compact,filepath,time):
          if compact == 'y':
              stringout = self.active+','+datetime2String_Num(time,'s')+','+str(self.alt)+','+str(self.azmu)+','+self.html+','+self.ra+','+self.dec+','+self.t_max_ut+','+self.tau+','+self.u_min+','+self.d_mag+','+self.i_bl+','+self.i_o+'\n'
@@ -299,6 +367,10 @@ def datetime2String_Num(time,outputType):
         h = h + m/60
         d = d + h/24
         output = [y,m,d]
+    elif outputType == 'hx':
+        m = m + s/60
+        h = h + m/60
+        output = h
     elif outputType == 'tonly':
         output = [h,mn,s]
     elif outputType == 'donly':
@@ -319,7 +391,7 @@ def datetime2String_Num(time,outputType):
         #Apply the time
         output = j + (h + (mn + (s/60.0))/60.0)/24.0
     return output
-def DMStoDeg(string,radQ):
+def DECtoDeg(string,radQ):
     if string[2] == ':':
         pn = 'pos'
         D = float(string[0:2])
@@ -330,9 +402,8 @@ def DMStoDeg(string,radQ):
         D = float(string[1:3])
         M = float(string[4:6])
         S = float(string[7:len(string)])
-    S = S/60
-    M = M/60 + S
-    D = D + M
+    M = M + S/60
+    D = D + M/60
     if pn == 'neg':
         D = 360-D
     if radQ =='y':
@@ -340,54 +411,43 @@ def DMStoDeg(string,radQ):
     else:
         out = D
     return out
-def dateFinder(NowQ,days,precision):
-    #days - out from now
-    #precision in minutes 
-    starthourUTC = '00:00'
-    endhourUTC = '09:00'
-    daysinmonths = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
-    #Note, code does not account for leap years at this stage.
-    if NowQ == 'y':
-        now = datetime.datetime.utcnow()
-        syear = now.year
-        smonth = now.month
-        sday = now.day
-        if now.hour >= 0 and now.hour <= 4:
-            if sday == 1:
-                if smonth == 1:
-                    syear = syear -1
-                    smonth = 12
-                else:
-                    smonth = smonth - 1
-                sday = daysinmonths[smonth]
-            else:
-                sday = sday - 1
-        shour = int(starthourUTC[0:2])
-        smin = int(starthourUTC[3:len(starthourUTC)])
-        ehour = int(endhourUTC[0:2])
-        emin = int(endhourUTC[3:len(endhourUTC)])
-        thour = shour
-        tmin = smin
-        tday = sday
-        tmonth = smonth
-        tyear = syear
-        UTCtimes = []
-        for i in range(1,days):
-            thour = shour
-            tmin = smin
-            if tday > daysinmonths[tmonth]:
-                tday = tday - daysinmonths[tmonth]
-                tmonth = tmonth + 1
-                if tmonth > 12:
-                    tmonth = 1
-                    tyear = tyear + 1
-            while thour < ehour or tmin < emin:
-                UTCtimes.append(datetime.datetime(tyear,tmonth,tday,thour,tmin))
-                tmin = tmin+precision
-                while tmin > 59:
-                    thour = thour + 1
-                    tmin = tmin - 60
-            tday = tday + 1
+def RAtoHours_Deg(string,ConvrtDegQ):
+    if string[2] == ':':
+        pn = 'pos'
+        H = float(string[0:2])
+        M = float(string[3:5])
+        S = float(string[6:len(string)])
     else:
-        print 'Sorry, this feature is not yet implemented.'
-    return UTCtimes
+        pn = 'neg'
+        H = float(string[1:3])
+        M = float(string[4:6])
+        S = float(string[7:len(string)])
+    M = M + S/60
+    H = H + M/60
+    if ConvrtDegQ == 'y':
+        out = H*15
+    else:
+        out = H
+    return out
+def daysFromJ2000(UTC):
+    #input is a datetime object in UTC
+    
+    
+    return days
+def LST_finder(d,Ut,lonng):
+    # Based upon the number of days from the epoch J2000
+#==============================================================================
+# LST = 100.46 + 0.985647 * d + long + 15*UT
+# 
+#       d    is the days from J2000, including the fraction of
+#            a day
+#       UT   is the universal time in decimal hours
+#       long is your longitude in decimal degrees, East positive.
+#       
+# Add or subtract multiples of 360 to bring LST in range 0 to 360 degrees.
+# and this formula gives your local siderial time in degrees. You can divide by 
+# 15 to get your local siderial time in hours, but often we leave the figure in 
+# degrees. The approximation is within 0.3 seconds of time for dates within 100 
+# years of J2000.
+#==============================================================================
+
